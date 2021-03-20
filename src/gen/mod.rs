d39fc104b6c585971df6794424d7fc3ce2178b61
Eliot Courtney <edgeworth.dev@gmail.com>
2021-03-20 22:24:11 +0900

    Implement order crossover.

--- src/gen/mod.rs
+++ src/gen/mod.rs
@@ -6,6 +6,6 @@ pub mod unevaluated;
 #[derive(Debug, Clone, PartialOrd, PartialEq)]
 pub struct Params {
     // Conventionally, the first element will be the weight of doing no mutation or crossover.
-    mutation: Vec<f64>,
-    crossover: Vec<f64>,
+    pub mutation: Vec<f64>,
+    pub crossover: Vec<f64>,
 }
